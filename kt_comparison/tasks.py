96505401346735dae36c452c9877fccdc03937f7
Maks Dobzhinetskii <dm.python.dev.wwl@gmail.com>
2024-01-03 12:15:14 +0200

    fixed: date range

--- kt_comparison/tasks.py
+++ kt_comparison/tasks.py
@@ -14,17 +14,17 @@ from kt_comparison.services.third_kt import ThirdKt
 def run_script():
     today = datetime.now()
 
-    # Визначаємо перший день попереднього місяця
-    if today.month == 1:
-        date_from = today.replace(year=today.year - 1, month=12, day=1)
-        date_to = date_from + timedelta(days=30)
-    else:
-        date_from = today.replace(month=today.month - 1, day=1)
-        date_to = date_from.replace(month=date_from.month + 1, day=1) - timedelta(days=1)
+    # # Визначаємо перший день попереднього місяця
+    # if today.month == 1:
+    #     date_from = today.replace(year=today.year - 1, month=12, day=1)
+    #     date_to = date_from + timedelta(days=30)
+    # else:
+    #     date_from = today.replace(month=today.month - 1, day=1)
+    #     date_to = date_from.replace(month=date_from.month + 1, day=1) - timedelta(days=1)
 
-    # Форматування дат
-    date_from_formatted = date_from.strftime('%Y-%m-%d')
-    date_to_formatted = date_to.strftime('%Y-%m-%d')
+    # # Форматування дат
+    # date_from_formatted = date_from.strftime('%Y-%m-%d')
+    date_to_formatted = today.strftime('%Y-%m-%d')
     
     tm = TraffManager('2022-05-01', date_to_formatted)
     tm.download_file()
